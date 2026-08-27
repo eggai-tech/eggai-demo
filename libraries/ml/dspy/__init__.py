fa7422000a6aa92325e221b2e3ff6927b5568eb0
Mile Misan <mile.misan@egg-ai.com>
2026-08-27 15:55:17 +0200

    Revert "Merge pull request #25 from eggai-tech/fix-json-adapter-2"
    This reverts commit ad7a9e5b54e6e2ac119bd55be01001c4f55277ae, reversing
    changes made to e7280b9473d147faf4d1ff44beb53fa3027c60d1.

--- libraries/ml/dspy/__init__.py
+++ libraries/ml/dspy/__init__.py
@@ -4,11 +4,10 @@ DSPy-specific utilities.
 This module provides DSPy language model integration and optimizers.
 """
 
-from .language_model import LenientChatAdapter, TrackingLM, dspy_set_language_model
+from .language_model import TrackingLM, dspy_set_language_model
 from .optimizer import SIMBAOptimizer
 
 __all__ = [
-    "LenientChatAdapter",
     "SIMBAOptimizer",
     "TrackingLM",
     "dspy_set_language_model",
